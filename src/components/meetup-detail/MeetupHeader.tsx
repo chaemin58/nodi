bd46287fd7565fc3680a28c17a2c42960a6a11e1
chaemin58 <sulv0309@naver.com>
2026-09-10 13:36:44 +0900

    fix: 반응형 포인트 수정

--- src/components/meetup-detail/MeetupHeader.tsx
+++ src/components/meetup-detail/MeetupHeader.tsx
@@ -11,7 +11,7 @@ interface MeetupHeader {
 
 export function MeetupHeader({ groupTitle, meetupTitle, date, badge }: MeetupHeader) {
   return (
-    <div className="flex min-w-100 flex-col gap-2 lg:border-border-default w-full lg:py-8 lg:pl-8 lg:border lg:rounded-[29px] lg:bg-white">
+    <div className="flex min-w-100 flex-col gap-2 md:border-border-default w-full md:py-8 md:pl-8 md:border md:rounded-[29px] md:bg-white">
       <div className="text-text-placeholder text-sm">{groupTitle}</div>
       <div className="flex gap-3 md:gap-5 items-center ">
         <div className="text-xl md:text-3xl font-semibold">{meetupTitle}</div>
