3dd0f8db59b73f8f4b2aaee8bf2a4ad7bed17cee
chaemin58 <sulv0309@naver.com>
2026-09-10 20:34:10 +0900

    feat: 장소 추가 버튼

---
+++ src/components/meetup-detail/AddPlace.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import SquarePlusBtn from "@/assets/icon/squre-plus.svg";
+import { useState } from "react";
+import { AddPlaceModal } from "./AddPlaceModal";
+
+interface AddPlaceProps {
+  meetupId: string;
+}
+
+export function AddPlace({ meetupId }: AddPlaceProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  return (
+    <>
+      <div
+        className="w-full min-h-15 cursor-pointer overflow-hidden border-border-default rounded-2xl bg-surface border-2 text-gray-400 border-dashed p-5 flex justify-center items-center gap-3 self-stretch"
+        onClick={() => {
+          setIsOpen(true);
+        }}
+      >
+        <SquarePlusBtn className="w-5 " />
+        <div>장소 추가하기</div>
+      </div>
+      {isOpen && <AddPlaceModal meetupId={meetupId} onClose={() => setIsOpen(false)} />}
+    </>
+  );
+}
